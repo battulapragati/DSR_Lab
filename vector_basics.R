ab9a6de8ecf8d005f63cbcdca8593da4682ee006
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-09-23 14:44:17 +0530

    23/09/20

---
+++ vector_basics.R
@@ -0,0 +1,14 @@
+vec<-c(1,2,3,4)
+vec
+vec * 2
+vec/2
+vec %% 2
+vec+6
+vex<-c('one','two','three','four')
+vex
+class(vec)
+class(vex)
+max(c(2.3,-9,0,6.1))
+c(1,2)+c(8,9)
+c(2,3,4,5)^c(2,3)
+c(2,3,4,5)%/%c(2,3)
