7520d20febda8e1bf264a6357b23bd9b36d6af0a
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-09-30 15:32:55 +0530

    Lab program 1(b)- 30/09/20

---
+++ program_five.R
@@ -0,0 +1,9 @@
+getwd()
+sales_delim<-read.delim("Mar_Basket_Software.csv.csv",sep=',')
+sales_delim
+vec<-c(2,3,4,5,6)
+sales<-(sales_delim)
+sales<-cbind(sales_delim,size=vec)
+sales
+ncol(sales)
+colnames(sales)
