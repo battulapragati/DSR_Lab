5e066f244780e1851839a36edf034b7c400e1d6e
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-11-11 14:51:03 +0530

    Update practice_question1.R

--- lab7/practice_question1.R
+++ lab7/practice_question1.R
@@ -1,3 +1,11 @@
+  
+# Given
+# mean of sample, xmean = 11.0g / 100ml
+# std deviation of sample, sigma = 2.1g / 100 ml
+# mean of population = 10.2g / 100ml
+# H0 mean = 150
+# Ha Mean <> 150
+
 library(visualize)
 #install.packages("visualize")
 zvalue=(11-10.2)/(21/sqrt(55))
@@ -5,6 +13,6 @@ zvalue
 pnorm(zvalue)
 dnorm(zvalue)
 pnorm(2.825218) - pnorm(-2.825218)
-qnorm(0.05)
+qnorm(0.05)# z value at 95% CI
 visualize.norm(stat=zvalue,mu=0,sd=1,section="upper")
 visualize.norm(stat=c(2.825218,-2.825218),mu=0,sd=1,section="tails")
